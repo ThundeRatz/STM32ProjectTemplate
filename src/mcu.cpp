345a274fe93b20cf7fe54ef9a4b0ca994d09d3e6
Eduardo-Barreto <eduardopontobarreto@gmail.com>
2023-10-22 16:53:43 -0300

    🎨 Function definitions inside namespace
    Co-authored-by: Bruno1406 <bruno.lossmachado@gmail.com>

--- src/mcu.cpp
+++ src/mcu.cpp
@@ -15,8 +15,8 @@
  * Public Function Body Definitions
  *****************************************/
 
-void hal::mcu::init(void)
-{
+namespace hal {
+void mcu::init(void) {
     HAL_Init();
 
     SystemClock_Config();
@@ -24,12 +24,11 @@ void hal::mcu::init(void)
     MX_GPIO_Init();
 }
 
-void hal::mcu::sleep(uint32_t ms)
-{
+void mcu::sleep(uint32_t ms) {
     HAL_Delay(ms);
 }
 
-void hal::mcu::led_toggle(void)
-{
+void mcu::led_toggle(void) {
     HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+}
 }
