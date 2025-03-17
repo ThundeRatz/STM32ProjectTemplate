221672a0741593469808db00322595a73628514f
Eduardo-Barreto <eduardopontobarreto@gmail.com>
2025-03-17 08:45:54 -0300

    ♻️ Use pin and ports from cube

--- config/target.hpp
+++ config/target.hpp
@@ -1,6 +1,13 @@
+#ifndef TARGET_HPP
+#define TARGET_HPP
+
+#include <main.h>
 #include "gpio.hpp"
 
 const hal::Gpio::Config led_config = {
-    .port = GPIOC,
-    .pin = GPIO_PIN_13,
+    .port = LED_GPIO_Port,
+    .pin = LED_Pin,
 };
+
+
+#endif // TARGET_HPP
