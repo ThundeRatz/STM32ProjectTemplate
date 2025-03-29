2d6137d6b96f75e9b01e9757ccc1b7f64c36a5dd
Eduardo-Barreto <eduardopontobarreto@gmail.com>
2025-03-29 16:05:41 -0300

    🔇 Make docs in batch mode

--- cmake/targets.cmake
+++ cmake/targets.cmake
@@ -58,7 +58,7 @@ add_custom_target(rebuild_all
 
 add_custom_target(docs
     COMMAND cd ${CMAKE_CURRENT_SOURCE_DIR} && doxygen Doxyfile
-    COMMAND make -C ${CMAKE_CURRENT_SOURCE_DIR}/docs/latex || true
+    COMMAND yes Q | make -C ${CMAKE_CURRENT_SOURCE_DIR}/docs/latex || true
     COMMAND mv ${CMAKE_CURRENT_SOURCE_DIR}/docs/latex/refman.pdf ${CMAKE_CURRENT_SOURCE_DIR}/docs/
     COMMAND rm -rf ${CMAKE_CURRENT_SOURCE_DIR}/docs/latex
 )
